9d37c545c49d8e39ffb7bed06b1f5ed42d2d2f68
Jo8nny <76188321+Jo8nny@users.noreply.github.com>
2023-02-14 22:05:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,23 +7,57 @@ function scrollTo(element) {
   }
   
   var buttonAbout = document.querySelector('.button-about');
-  var buttonServices= document.querySelector('.button-services');
+  var buttonTakeTest= document.querySelector('.take-test-button');
   var buttonContact = document.querySelector('.button-contact');
 
 
 
   var about = document.querySelector('#about');
-  var services = document.querySelector('#services');
+  var test = document.querySelector('#test');
   var contact = document.querySelector('#contact');
   
   buttonAbout.addEventListener('click', () => {
     scrollTo(about);
   })
 
-  buttonServices.addEventListener('click', () => {
-    scrollTo(services);
+  buttonTakeTest.addEventListener('click', () => {
+    scrollTo(test);
   })
 
   buttonContact.addEventListener('click', () => {
     scrollTo(contact);
   })
+
+
+const quizForm = document.getElementById('testform');
+const submitButton = document.getElementById('submitButton');
+const resultsContainer = document.getElementById('results');
+
+submitButton.addEventListener('click', function() {
+  let score = 0;
+
+  // Question 1
+  if (testform.q1.value === 'b') {
+    score++;
+  }
+
+  // Question 2
+  if (testform.q2.value === 'c') {
+    score++;
+  }
+
+  // Question 3
+  if (testform.q3.value === 'c') {
+    score++;
+  }
+
+  // Question 4
+  if (testform.q4.value === 'a') {
+    score++;
+  }
+
+  // Display results
+  resultsContainer.innerHTML = `You scored ${score} out of 5.`;
+});
+
+
